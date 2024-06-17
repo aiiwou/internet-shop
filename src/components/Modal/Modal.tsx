8b1a4f555a272524290d3db31e784c02095f4c52
aiiwou <volgin.maxim2015@gmail.com>
2024-06-17 22:26:33 +0500

    Update Modal.tsx

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,21 +1,24 @@
-import { FC } from 'react'
-import styles from './Modal.module.css'
-import clsx from 'clsx'
+import { FC } from "react";
+import styles from "./Modal.module.css";
+import clsx from "clsx";
 type ModalProps = {
-    open: boolean,
-    close: () => void,
-    number: number
-}
+  open: boolean;
+  close: () => void;
+  number: number;
+};
 const Modal: FC<ModalProps> = (props) => {
-    const {open, close, number} = props
+  const { open, close, number } = props;
   return (
-    <div onClick={() => close()} className={clsx(styles.modal_container, open && styles.open )}>
-        <div onClick={(e) => e.stopPropagation()} className={styles.modal}>
-            <h1>УСПЕШНО</h1>
-            <p>Ваш номер заказа: {number}</p>
-        </div>
+    <div
+      onClick={() => close()}
+      className={clsx(styles.modal_container, open && styles.open)}
+    >
+      <div onClick={(e) => e.stopPropagation()} className={styles.modal}>
+        <h1>УСПЕШНО</h1>
+        <p>Ваш номер заказа: {number}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
